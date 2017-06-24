7371730be96a0e2a9cc891aca3f97e79890eea8e
Insaniator <masterrdonnelly@y7mail.com>
2017-06-24 16:42:11 +0800

    Add files via upload

---
+++ Unit Project Test 1/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+
+namespace Unit_Project_Test_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x, y;
+            int expected;
+
+            var Test_Vehicle = new Vehicle()
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cars_OpensourcetestingCluster;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double distance = 100;
+            double fuel_used = 20;
+            double expected = 20;
+            var Test_Vehicle = new Vehicle(1, "Ford", "GTX", "2007", "dkedfe33", distance, 0, fuel_used, 0, 0);
+            double actual = Test_Vehicle.Fuel_Economy();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int distance = 100;
+            int vehicle_id = 1;
+            int fuel_used = 20;
+            double expected = 100;
+            
+            var test1 = new PerKmRental(1, 100, 20);
+
+            double actual = test1.Price();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
